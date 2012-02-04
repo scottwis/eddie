84806d2596e67fe7bfaefcc3e65c0f0cf93b820d
Scott Wisniewski <scott@scottdw2.com>
2012-02-04 09:36:44 -0500

    Import NUnit-2.5.10.11092 sources into the vendor/nunit branch.
    Issue #8.

---
+++ third-party/nunit/src/tests/test-assembly/ParameterizedTestFixture.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace NUnit.TestData
+{
+    [TestFixture(1)]
+    [TestFixture(2)]
+    public class ParameterizedTestFixture
+    {
+        [Test]
+        public void MethodWithoutParams()
+        {
+        }
+
+        [TestCase(10,20)]
+        public void MethodWithParams(int x, int y)
+        {
+        }
+    }
+
+    [TestFixture(Category = "XYZ")]
+    public class TestFixtureWithSingleCategory
+    {
+    }
+
+    [TestFixture(Category = "X,Y,Z")]
+    public class TestFixtureWithMultipleCategories
+    {
+    }
+}
